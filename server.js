0404666c8d3ced650fde28d92a8afd88c1832bda
Marcin <mistamasta7@gmail.com>
2022-04-05 22:01:42 +0200

    Develop free seats feature & bug fixes

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const path = require('path');
+const socket = require('socket.io');
 
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
 
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
@@ -13,7 +18,7 @@ app.use(express.json());
 app.use(cors());
 
 
-app.listen(process.env.PORT || 8000, () => {
+const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
 
@@ -28,4 +33,13 @@ app.get('*', (req, res) => {
 
 app.use((req, res) => {
   res.status(404).json({message: '404 not found...'});
+});
+
+
+const io = socket(server);
+io.on('connection', (socket) => {
+  console.log('a user connected ' + socket.id);
+  socket.on('disconnect', () => {
+    console.log('Socket ' + socket.id + ' left');
+  });
 });
